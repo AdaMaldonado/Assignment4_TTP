bf531d5e9ff58a48b4ec3fe13b8749170804667b
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-07 18:14:36 -0500

    Display messages while the buttons are clicked

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let firstButtonEl = document.getElementById("firstButton")
+let secondButtonEl = document.getElementById("secondButton")
 
+let firstTextEl = document.getElementById("firstText")
+let secondTextEl = document.getElementById("secondText")
+
+firstButtonEl.addEventListener(
+  "click", 
+  function() {
+    firstTextEl.textContent("I'm right")
+  }
+)
+
+secondButtonEl.addEventListener(
+  "click", 
+  function() {
+    secondTextEl.textContent("No, I'm right!")
+  }
+)
